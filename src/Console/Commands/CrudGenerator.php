900c746358d77fee17de6b3898e66039e507ff0a
hossam <hossam0soliuman@gmail.com>
2023-08-29 00:26:57 +0300

    enhance the CrudGenerator command with more features and improved user interaction

--- src/Console/Commands/CrudGenerator.php
+++ src/Console/Commands/CrudGenerator.php
@@ -4,52 +4,54 @@ namespace Hossam\Licht\Console\Commands;
 
 use Hossam\Licht\Generators\ControllerGeneratore;
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class CrudGenerator extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'licht:model {name}';
+    protected $description = 'Generate CRUD operations for a model';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'generating CRUDs';
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
-        $isThereMore = 'yes';
-        $fields = [];
-        while ($isThereMore == 'yes') {
-            $fieldType = $this->choice(
-                'Choose field type ',
-                ['string', 'integer', 'text', 'foreignId'],
-                0
-            );
-            $fieldName = $this->ask('Enter field name', 'name');
+        $this->info('Welcome to the CRUD Generator');
+        $modelName = $this->argument('name');
 
-            $fields[$fieldName] = $fieldType;
-            $isThereMore = $this->choice(
-                'Are there any more fields?',
-                ['yes', 'no'],
-                0
-            );
-        }
-        $model = $this->argument('name');
+        $fields = $this->gatherFields();
 
         $controller = new ControllerGeneratore;
-        $controller->create($model);
+        $controller->create($modelName, $fields);
 
-        return Command::SUCCESS;
+        $this->info("CRUD operations generated for {$modelName}.");
+    }
+
+    protected function gatherFields()
+    {
+        $fields = [];
+        $askForFields = true;
+
+        while ($askForFields) {
+            $fieldType = $this->askFieldType();
+            $fieldName = $this->ask('Enter field name', 'name');
+            
+            $fields[$fieldName] = $fieldType;
+
+            if (!$this->confirm('Add more fields?', true)) {
+                $askForFields = false;
+            }
+        }
+
+        return $fields;
+    }
+
+    protected function askFieldType()
+    {
+        $question = new ChoiceQuestion(
+            'Choose field type',
+            ['string', 'integer', 'text', 'foreignId'],
+            0
+        );
+
+        return $this->choice($question->getQuestion(), $question->getChoices(), 0);
     }
 }
